df3521b86adc135b4fc7e94ebf233ec669be2124
robertjlooby <robertjlooby@gmail.com>
2013-06-26 17:30:34 -0500

    added play loop, run 'ruby play.rb' to play a game

---
+++ play.rb
@@ -0,0 +1,10 @@
+require_relative 'game_io'
+require_relative 'game_logic'
+
+if ARGV[0].nil?
+    code = "WWWW"
+else
+    code = ARGV[0].strip.upcase.match(/[RGBWYO]*/).to_s
+    code = "WWWW" unless code.length == 4
+end
+GameLogic.game(code, 10, GameIO::Reader.new, GameIO::Writer.new)

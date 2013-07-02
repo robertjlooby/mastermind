8fc6330714211d87b9a6c8c9167de43748cebe1f
robertjlooby <robertjlooby@gmail.com>
2013-07-02 18:01:59 -0500

    added script to play on the command line (each turn runs in 2 separate threads)

---
+++ play_human.rb
@@ -0,0 +1,19 @@
+require_relative 'game_logic'
+require_relative 'human_player'
+require_relative 'game'
+
+if ARGV[0].nil?
+    code = "RGBWYO".split("").repeated_permutation(4).to_a.sample.join
+else
+    code = ARGV[0].strip.upcase.match(/[RGBWYO]*/).to_s
+    code = "WWWW" unless code.length == 4
+end
+
+p2l_read, p2l_write = IO.pipe
+l2p_read, l2p_write = IO.pipe
+
+game = Game.new(10, code, HumanPlayer, GameLogic, p2l_read, p2l_write, l2p_read, l2p_write)
+outcome = game.play_game
+
+puts "You #{outcome}!"
+puts "Code was #{code}"

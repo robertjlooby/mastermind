a4ad793a7d2c7ca6db090b627893b1d6281bdc1a
robertjlooby <robertjlooby@gmail.com>
2013-07-02 19:51:50 -0500

    changed GameLogic from module to class, turns/game loop now handled by Game class

--- game_logic.rb
+++ game_logic.rb
@@ -1,8 +1,15 @@
-module GameLogic
-    def self.guess(guess, actual)
+class GameLogic
+    attr_accessor :code
+    def initialize(code, i_stream, o_stream)
+        @code = code
+        @i_stream = i_stream
+        @o_stream = o_stream
+    end
+
+    def respond
         red = white = 0
-        guess = guess.split("")
-        actual = actual.split("")
+        guess = @i_stream.gets.chomp.split("")
+        actual = @code.split("")
 
         (0 ... guess.length).each do |index|
             if guess[index] == actual[index]
@@ -20,22 +27,7 @@ module GameLogic
             end
         end
 
+        @o_stream.puts [red, white].to_s
         [red, white]
-    end
-
-    def self.game(actual, turns, reader, writer)
-        turns.times do
-            result = self.turn(actual, reader, writer)
-            return :win if result == [4, 0]
-        end
-        :lose
-    end
-
-    def self.turn(actual, reader, writer)
-        writer.ask_for_guess
-        guess = reader.read_guess
-        result = self.guess(guess, actual)
-        writer.write_response result
-        result
     end
 end
--- game_logic_spec.rb
+++ game_logic_spec.rb
@@ -1,170 +1,148 @@
 require 'rspec'
+require 'stringio'
 require_relative 'game_logic'
 
 describe GameLogic do
     before do
-        @reader = MockReader.new
-        @writer = MockWriter.new
+        @p2l_reader = StringIO.new
+        @l2p_writer = StringIO.new
+        @code = "WBRG"
+        @logic = GameLogic.new @code, @p2l_reader, @l2p_writer
     end
 
     it "module exists" do
     end
 
+    it "has an initial code of WBRG" do
+        @logic.code.should == "WBRG"
+    end
+
     it "returns no pegs when no colors are correct" do
-        GameLogic.guess("RRRR", "WWWW").should == [0, 0]
+        @logic.code = "WWWW"
+        @p2l_reader.puts("RRRR")
+        @p2l_reader.rewind
+
+        @logic.respond.should == [0, 0]
+		@l2p_writer.rewind
+		@l2p_writer.gets.chomp.should == [0, 0].to_s
     end
 
     it "returns a red peg when one peg is correct in position and color" do
-        GameLogic.guess("WRRR", "WWWW").should == [1, 0]
+        @logic.code = "WWWW"
+        @p2l_reader.puts("WRRR")
+        @p2l_reader.rewind
+
+        @logic.respond.should == [1, 0]
+		@l2p_writer.rewind
+		@l2p_writer.gets.chomp.should == [1, 0].to_s
     end
 
     it "returns two red pegs when two pegs are correct in position and color" do
-        GameLogic.guess("WWRR", "WWWW").should == [2, 0]
+        @logic.code = "WWWW"
+        @p2l_reader.puts("WWRR")
+        @p2l_reader.rewind
+
+        @logic.respond.should == [2, 0]
+		@l2p_writer.rewind
+		@l2p_writer.gets.chomp.should == [2, 0].to_s
     end
 
     it "returns three red pegs when three pegs are correct in position and color" do
-        GameLogic.guess("WWWR", "WWWW").should == [3, 0]
+        @logic.code = "WWWW"
+        @p2l_reader.puts("WWWR")
+        @p2l_reader.rewind
+
+        @logic.respond.should == [3, 0]
+		@l2p_writer.rewind
+		@l2p_writer.gets.chomp.should == [3, 0].to_s
     end
 
     it "returns one white peg when one peg is correct in color but not position" do
-        GameLogic.guess("WBBB", "RRRW").should == [0, 1]
+        @logic.code = "RRRW"
+        @p2l_reader.puts("WBBB")
+        @p2l_reader.rewind
+
+        @logic.respond.should == [0, 1]
+		@l2p_writer.rewind
+		@l2p_writer.gets.chomp.should == [0, 1].to_s
     end
 
     it "returns two white pegs when two pegs are correct in color but not position" do 
-        GameLogic.guess("WWBB", "RRWW").should == [0, 2]
+        @logic.code = "RRWW"
+        @p2l_reader.puts("WWBB")
+        @p2l_reader.rewind
+
+        @logic.respond.should == [0, 2]
+		@l2p_writer.rewind
+		@l2p_writer.gets.chomp.should == [0, 2].to_s
     end
 
     it "returns one white peg when two pegs of a color are present in the guess but only one is in the solution" do
-        GameLogic.guess("WWBB", "RRRW").should == [0, 1]
+        @logic.code = "RRRW"
+        @p2l_reader.puts("WWBB")
+        @p2l_reader.rewind
+
+        @logic.respond.should == [0,1]
+		@l2p_writer.rewind
+		@l2p_writer.gets.chomp.should == [0, 1].to_s
     end
     it "returns two red pegs when guess is four of same color but only two pegs are correct in color and position" do
-        GameLogic.guess("WWWW", "WWBB").should == [2, 0]
+        @logic.code = "WWBB"
+        @p2l_reader.puts("WWWW")
+        @p2l_reader.rewind
+
+        @logic.respond.should == [2, 0]
+		@l2p_writer.rewind
+		@l2p_writer.gets.chomp.should == [2, 0].to_s
     end
 
     it "returns four red pegs when guess is same as actual" do
-        GameLogic.guess("WBRB", "WBRB").should == [4, 0]
+        @logic.code = "WBRB"
+        @p2l_reader.puts("WBRB")
+        @p2l_reader.rewind
+
+        @logic.respond.should == [4, 0]
+		@l2p_writer.rewind
+		@l2p_writer.gets.chomp.should == [4, 0].to_s
     end
 
     it "returns four white pegs when guess is correct in colors but not positions" do
-        GameLogic.guess("WBRB", "BWBR").should == [0, 4]
+        @logic.code = "BWBR"
+        @p2l_reader.puts("WBRB")
+        @p2l_reader.rewind
+
+        @logic.respond.should == [0, 4]
+		@l2p_writer.rewind
+		@l2p_writer.gets.chomp.should == [0, 4].to_s
     end
 
     it "returns two white and two red pegs when two pegs are correct in color and position and two pegs are correct in color" do
-        GameLogic.guess("WBRB", "WBBR").should == [2, 2]
+        @logic.code = "WBBR"
+        @p2l_reader.puts("WBRB")
+        @p2l_reader.rewind
+
+        @logic.respond.should == [2, 2]
+		@l2p_writer.rewind
+		@l2p_writer.gets.chomp.should == [2, 2].to_s
     end
 
     it "returns one white and two red pegs when two pegs are correct in color and position and one peg are correct in color" do
-        GameLogic.guess("WBYB", "WBBR").should == [2, 1]
+        @logic.code = "WBBR"
+        @p2l_reader.puts("WBYB")
+        @p2l_reader.rewind
+
+        @logic.respond.should == [2, 1]
+		@l2p_writer.rewind
+		@l2p_writer.gets.chomp.should == [2, 1].to_s
     end
 
     it "returns two white and one red pegs when one peg is correct in color and position and two pegs are correct in color" do
-        GameLogic.guess("WYRB", "WBBR").should == [1, 2]
-    end
+        @logic.code = "WBBR"
+        @p2l_reader.puts("WYRB")
+        @p2l_reader.rewind
 
-    it "for a single turn: reads once, asks for guess once, writes response once. response is correct" do
-        @reader.responses = ["WBBG"]
-        GameLogic.turn("WYRB", @reader, @writer)
-
-        @writer.times_asked_for_guess.should == 1
-        @writer.times_wrote_response.should == 1
-        @reader.times_read.should == 1
-        @writer.responses[0].should == [1, 1]
-    end
-
-    it "for three turns: asks for guess, reads, writes response three times each. responses are correct" do
-        @reader.responses = ["WBBG", "WYBR", "YWRR"]
-        GameLogic.turn("WYRB", @reader, @writer)
-        GameLogic.turn("WYRB", @reader, @writer)
-        GameLogic.turn("WYRB", @reader, @writer)
-
-        @writer.times_asked_for_guess.should == 3
-        @writer.times_wrote_response.should == 3
-        @reader.times_read.should == 3
-        @writer.responses[0].should == [1, 1]
-        @writer.responses[1].should == [2, 2]
-        @writer.responses[2].should == [1, 2]
-    end
-
-    it "for a ten turn game: asks for guess, reads, writes response ten times each. responses are correct" do
-        @reader.responses = ["WBBG", "WYBR", "YWRR", 
-                             "WBBG", "WYBR", "YWRR", 
-                             "WBBG", "WYBR", "YWRR", 
-                             "WYRB"]
-        GameLogic.game("WYRB", 10, @reader, @writer)
-
-        @writer.times_asked_for_guess.should == 10
-        @writer.times_wrote_response.should == 10
-        @reader.times_read.should == 10
-        @writer.responses[0].should == [1, 1]
-        @writer.responses[1].should == [2, 2]
-        @writer.responses[2].should == [1, 2]
-        @writer.responses[3].should == [1, 1]
-        @writer.responses[4].should == [2, 2]
-        @writer.responses[5].should == [1, 2]
-        @writer.responses[6].should == [1, 1]
-        @writer.responses[7].should == [2, 2]
-        @writer.responses[8].should == [1, 2]
-        @writer.responses[9].should == [4, 0]
-    end
-
-    it "returns :win when the game is won on the first turn" do
-        @reader.responses = ["WYRB"]
-        result = GameLogic.game("WYRB", 10, @reader, @writer)
-
-        @writer.times_asked_for_guess.should == 1
-        @writer.times_wrote_response.should == 1
-        @reader.times_read.should == 1
-        @writer.responses[0].should == [4, 0]
-        result.should == :win
-    end
-
-    it "returns :win when the game is won on the last turn" do
-        @reader.responses = ["WBBG", "WYBR", "YWRR", 
-                             "WBBG", "WYBR", "YWRR", 
-                             "WBBG", "WYBR", "YWRR", 
-                             "WYRB"]
-        result = GameLogic.game("WYRB", 10, @reader, @writer)
-
-        result.should == :win
-    end
-
-    it "returns :lose when the game is not won" do
-        @reader.responses = ["WBBG", "WYBR", "YWRR", 
-                             "WBBG", "WYBR", "YWRR", 
-                             "WBBG", "WYBR", "YWRR", 
-                             "WBRB"]
-        result = GameLogic.game("WYRB", 10, @reader, @writer)
-
-        result.should == :lose
-    end
-
-end
-
-class MockReader
-    attr_accessor :times_read, :responses
-    def initialize
-        @times_read = 0
-    end
-
-    def read_guess
-        @times_read += 1
-        @responses.shift
-    end
-end
-
-class MockWriter
-    attr_accessor :times_asked_for_guess, :times_wrote_response, :responses
-    def initialize
-        @times_asked_for_guess = @times_wrote_response = 0
-        @responses = []
-    end
-
-    def ask_for_guess
-        @times_asked_for_guess += 1
-    end
-
-    def write_response(response)
-        @responses.push response
-        @times_wrote_response += 1
+        @logic.respond.should == [1, 2]
+		@l2p_writer.rewind
+		@l2p_writer.gets.chomp.should == [1, 2].to_s
     end
 end
